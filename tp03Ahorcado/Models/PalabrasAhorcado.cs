2d8fffc88fa36ddbd72aea4b0970a8056002a63f
MANUEL DO REGO BRICCHETTO <50155748@ort>
2026-06-03 08:37:02 -0300

    constructormodelohecho

---
+++ tp03Ahorcado/Models/PalabrasAhorcado.cs
@@ -0,0 +1,21 @@
+namespace tp03Ahorcado.Models;
+
+public class PalabrasAhorcado
+{
+    private List<string> palabras;
+
+    public PalabrasAhorcado
+    {
+        palabras.Add("ABANICOS", "ELEFANTE", "FELICIDAD", "INFORMATICA", "CARRUAJE", "TERREMOTO", "LABERINTO", "MARIPOSA", "AVENTURA", "ARQUITECTO")
+    }
+
+
+
+
+
+
+
+
+
+
+}
