dccedf5831f21a3194aee0c0aaba08e00f287243
MANUEL DO REGO BRICCHETTO <50155748@ort>
2026-06-05 08:20:01 -0300

    AjusteUnPocoElJS

--- tp03Ahorcado/wwwroot/js/site.js
+++ tp03Ahorcado/wwwroot/js/site.js
@@ -1,7 +1,7 @@
 ﻿const palabra = document.getElementById("palabraOculta").value;
 
 let palabraOculta = [];
-let intentos = 9;
+let intentos = 10;
 
 for (let i = 0; i < palabra.length; i++) {
     palabraOculta.push("_");
@@ -13,30 +13,44 @@ function arriesgarLetra()
 {
     const letra = document.getElementById("ingLetra").value.toUpperCase();
     let correcto = false;
-if(intentos > 0)
+    if(intentos > 0)
     {
     for(let i = 0; i<palabra.length; i++)
     {
         if (palabra[i] == letra)
             {
                 palabraOculta[i] = letra;
-                document.getElementById("respuesta").innerHTML = "Bien!  Te quedan " + (intentos + 1 ) + " intentos.";
+                document.getElementById("respuesta").innerHTML = "Bien!  Te quedan " + (intentos) + " intentos.";
                 correcto = true;
+            }
+    }
+
+    if(!correcto)
+        {
+
+         intentos--;
+
+        if (intentos > 1)
+            {
+                document.getElementById("respuesta").innerHTML = "Mal. Te quedan " + (intentos) + " intentos.";
+            }
+        else if (intentos == 1)
+            {
+                document.getElementById("respuesta").innerHTML = "Mal. Te queda " + (intentos) + " intento.";
+            }
+        else
+            {
+                document.getElementById("respuesta").innerHTML = "Derrota!";
             }
 
 
-    
-    }
-    if(correcto == false){
-         intentos--;
-      document.getElementById("respuesta").innerHTML = "Mal. Te quedan " + (intentos + 1) + " intentos.";
        
     }
 
     document.getElementById("miForm").reset();
 
-document.getElementById("texto").innerHTML = "Palabra: " + palabraOculta.join(" ") + ".";
-}
+    document.getElementById("texto").innerHTML = "Palabra: " + palabraOculta.join(" ") + ".";
+    }
 else
     {
         document.getElementById("respuesta").innerHTML = "Derrota!";
@@ -45,6 +59,7 @@ else
 if(palabraOculta.includes("_") == false)
         {
             document.getElementById("respuesta").innerHTML = "Victoria!";
+            intentos = 0;
         }
 
 }
